29cbc73d9238c09b8e17371d4831464a3c5bc512
Servidor <emisor.tarea@gmail.com>
2020-10-23 15:00:24 +0000

    Servidor

--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,14 @@ Route::resource('grado', 'escuela\catalogo\GradoController'); //REALIZADO EL 12/
 Route::resource('gradoSeccion', 'escuela\catalogo\GradoSeccionController')->except(['show']); //REALIZADO EL 12/09
 Route::resource('mes','escuela\catalogo\MesController'); //REALIZADO EL 12/09
 Route::resource('seccion', 'escuela\catalogo\SeccionController')->except(['show']); //REALIZADO EL 13/09
-Route::resource('tipoFondo', 'escuela\catalogo\tipoFondoController')->except(['show']); //REALIZADO EL 13/09
-Route::resource('tipoPagoAlumno', 'escuela\catalogo\tipoPagoAlumnoController'); //REALIZADO EL 13/09
+Route::resource('tipoFondo', 'escuela\catalogo\TipoFondoController')->except(['show']); //REALIZADO EL 13/09
+Route::resource('tipoPagoAlumno', 'escuela\catalogo\TipoPagoAlumnoController'); //REALIZADO EL 13/09
 
 Route::resource('rol', 'escuela\seguridad\RolController'); //REALIZADO EL 14/09
 Route::resource('usuario', 'escuela\seguridad\UsuarioController'); //REALIZADO EL 14/09
 
 Route::resource('alumno', 'escuela\sistema\AlumnoController')->except(['show']); //REALIZADO EL 14/09
-Route::resource('alumnoGrado', 'escuela\sistema\alumnoGradoController')->except(['create', 'store', 'show', 'edit', 'update', 'destroy']); //REALIZADO EL 14/09
+Route::resource('alumnoGrado', 'escuela\sistema\AlumnoGradoController')->except(['create', 'store', 'show', 'edit', 'update', 'destroy']); //REALIZADO EL 14/09
 Route::resource('catedratico', 'escuela\sistema\CatedraticoController'); //REALIZADO EL 14/09
 Route::resource('catedraticoCurso','escuela\sistema\CatedraticoCursoController')->except(['create', 'edit', 'update']); //REALIZADO EL 15/09
 Route::resource('catedraticoCurso','escuela\sistema\CatedraticoCursoController')->except(['create', 'edit', 'update']); //REALIZADO EL 15/09
@@ -44,7 +44,7 @@ Route::resource('inscripcion', 'escuela\sistema\InscripcionController')->except(
 Route::name('inscripcion.create_siguiente')->get('create_siguiente/inscripcion', 'escuela\sistema\InscripcionController@create_siguiente'); //REALIZADO EL 
 Route::name('inscripcion.store_siguiente')->post('store_siguiente/inscripcion', 'escuela\sistema\InscripcionController@store_siguiente'); //REALIZADO EL 
 Route::resource('mensualidad', 'escuela\sistema\MensualidadController')->except(['create', 'show', 'edit', 'update', 'destroy']); //REALIZADO EL 16/09
-Route::resource('pagoCatedratico','escuela\sistema\pagoCatedraticoController')->except(['create', 'edit', 'update', 'destroy']); //REALIZADO EL 17/09
+Route::resource('pagoCatedratico','escuela\sistema\PagoCatedraticoController')->except(['create', 'edit', 'update', 'destroy']); //REALIZADO EL 17/09
 Route::resource('persona','escuela\sistema\PersonaController')->except(['show']); //REALIZADO EL 17/09
 Route::resource('municipio','escuela\catalogo\MunicipioController'); //REALIZADO EL 17/09
 Route::resource('promedio','escuela\sistema\PromedioController')->except(['create', 'store', 'show', 'edit', 'update', 'destroy']); //REALIZADO EL 17/09
